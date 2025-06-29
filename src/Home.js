ca2c27d310f2964e94aedbd8ed45f1fd7ac9d44d
Jasec <jasec_7@hotmail.com>
2025-06-28 21:40:03 -0700

    create Home component

---
+++ src/Home.js
@@ -0,0 +1,13 @@
+import React from "react";
+
+function Home(){
+    return(
+        <div>
+            <h1>Welcome to Player Skill Tracker</h1>
+            <hr/>
+            <p>Search players and track their performance with detailed scoiting notes</p>
+        </div>
+    )
+};
+
+export default Home
