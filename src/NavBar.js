5cda5b05e24f404eef0595748ca49453b082da6b
Jasec <jasec_7@hotmail.com>
2025-06-29 21:46:43 -0700

    create NavBar component

---
+++ src/NavBar.js
@@ -0,0 +1,24 @@
+import "./App.css";
+import { NavLink } from "react-router-dom"; 
+
+function NavBar(){
+    return(
+        <nav>
+        <NavLink
+        to="/"
+        className="nav-link"
+        >
+            Home
+        </NavLink>
+        <NavLink
+        to="/players"
+        className="nav-link"
+        >
+            Players
+        </NavLink>
+       </nav>
+    )
+
+}
+
+export default NavBar
--- src/PlayerDetails.js
+++ src/PlayerDetails.js
@@ -3,7 +3,13 @@ import React from "react";
 function PlayerDetails({id, name, team, position, image}){
     return(
         <div className="details">
+            <img src={image} alt={name} />
             <h2>{name}</h2>
+            <p>Team:{team}</p>
+            <p>Position:{position}</p>
+            <p>View Performance Notes:</p>
+
+
         </div>
 
     )
