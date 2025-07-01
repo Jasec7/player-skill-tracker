4dea5780da2015ff8cadc397c52072d24c44a3f0
Jasec <jasec_7@hotmail.com>
2025-07-01 01:09:47 -0700

    update NoteForm

--- src/NoteForm.js
+++ src/NoteForm.js
@@ -1,4 +1,4 @@
-import React, {useEffect, useState} from "react";
+import React, {useState} from "react";
 
 function NoteForm(){
     const [formData, setFormData] = useState({
@@ -7,9 +7,32 @@ function NoteForm(){
     });
 
     function handleSubmit(e){
-        e.prentDefault();
-    }
+        e.preventDefault();
+
+        fetch("http://localhost:3001/notes",{
+                method:"POST",
+                headers:{
+                    "Content-Type":"Application/JSON"
+                },
+                body:JSON.stringify(formData)
+            })
+            .then(r => r.json())
+            .then((newNote) => {console.log(newNote);
+                setFormData({scout:"", comment:""})
+            })
+        }
+
     return(
+        <div className="new-note-form">
+            <h2>Feedback</h2>
+            <form onSubmit={handleSubmit}>
+                <input type='text' name='scout' placeholder='scout'
+                value=""/>
+                <input type='text' name='comment' placeholder='comment'
+                value=""/>
+                <button type='submit'>Add Coment</button> 
+            </form>
+        </div>
 
     )
 }
