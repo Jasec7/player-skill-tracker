5c6bc3a7e72de4937271899fbd21ea9d87df6c6b
Jasec <jasec_7@hotmail.com>
2025-07-02 02:21:49 -0700

    add css

--- src/PlayerDetails.js
+++ src/PlayerDetails.js
@@ -3,11 +3,12 @@ import {Link} from 'react-router-dom';
 function PlayerDetails({id, name, team, position, image}){
     return(
         <div className="details">
-            <img src={image} alt={name}/>
+            <br/>
+            <img src={image} alt={name} className="player-image"/>
             <h2>{name}</h2>
             <p>Team: {team}</p>
             <p>Position: {position}</p>
-            <Link to={`/players/${id}/notes`}>View Performance Notes</Link>
+            <Link to={`/players/${id}/notes`}>View Performance Notes</Link >
         </div>
 
     )
