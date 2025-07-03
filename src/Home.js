4c1d97fac56da23e6b0a9e83c29929eea7d7405c
Jasec <jasec_7@hotmail.com>
2025-07-03 02:25:58 -0700

    add logo animation

--- src/Home.js
+++ src/Home.js
@@ -7,9 +7,17 @@ function Home(){
             <h1>Welcome to Player Skill Tracker</h1>
             <hr/>
             <p>Search players and track their performance with detailed scouting notes</p>
+            <img 
+             src="https://static.vecteezy.com/system/resources/previews/025/257/277/original/soccer-ball-cartoon-png.png" 
+                alt="Soccer Ball" 
+                className="logo"
+                style={{ width: "150px", marginBottom: "20px" }} 
+            />
         </div>
         </>
     )
 };
 
 export default Home
+
+//https://static.vecteezy.com/system/resources/previews/025/257/277/original/soccer-ball-cartoon-png.png
